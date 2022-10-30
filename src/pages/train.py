10a9962c9edcd887899e516c319c9de873c08d26
koraypoyraz <koraypoyraz1@gmail.com>
2022-10-30 15:27:37 +0100

    - fix save button (pre-save vs post-save) after evaluation - added metrics visuals before saving the model

--- src/pages/train.py
+++ src/pages/train.py
@@ -5,6 +5,7 @@ from keras.optimizers import Adam
 from src.data.exampleData import ExampleData
 import plotly.graph_objects as go
 import numpy as np
+from src.pages.evaluation import model_evaluation, get_threshold, plot_metrics
 
 name = 'Train'
 
@@ -13,29 +14,37 @@ def app():
     if 'HISTORY' not in st.session_state:
         st.session_state['HISTORY'] = {'train_loss': [0], 'val_loss': [0]}
 
+    if 'MODEL' not in st.session_state:
+        st.session_state['MODEL'] = None
+
     st.header('Train an Anomaly Detector with AutoEncoder')
 
     epochs = st.slider('How many epochs to train?', 10, 100, 20)
+
+    exampleData = ExampleData()
+    train_data, test_data, normal_train, normal_test, anom_train, anom_test = exampleData.get_all_training_data()
 
     with st.container():
         buttons = st.columns(2)
 
         with buttons[0]:
-            to_save = st.checkbox('Save network')
+            st.button('Train', key=random(), on_click=train_model, args=(epochs, train_data, normal_train, test_data))
 
         with buttons[1]:
-            st.button('Train', key=random(), on_click=train_model, args=(epochs, to_save))
+            save_clicked = st.button('Save?')
+            if st.session_state['MODEL'] is not None:
+                if save_clicked:
+                    st.session_state['MODEL'].save(f'./model/anomaly-detector-{epochs}')
+                    st.success('Successfully saved', icon="✅")
 
-    plot(epochs)
+    if st.session_state['MODEL'] is not None:
+        plot_metric(st.session_state['MODEL'], normal_train, test_data, exampleData.test_labels)
+        plot_loss(epochs)
 
 
-def train_model(epochs, to_save):
+def train_model(epochs, train_data, normal_train, test_data):
     optimizer = Adam()
     loss = 'mae'
-
-    exampleData = ExampleData()
-
-    train_data, test_data, normal_train, normal_test, anom_train, anom_test = exampleData.get_all_training_data()
 
     units = train_data.shape[-1]
 
@@ -47,14 +56,17 @@ def train_model(epochs, to_save):
                         validation_data=(test_data, test_data),
                         shuffle=True)
 
-    if to_save:
-        model.save(f'anomalyDetectorApp/model/anomaly-detector-{epochs}')
-        st.success('Successfully saved', icon="✅")
-
     st.session_state['HISTORY'] = {'train_loss': history.history["loss"], 'val_loss': history.history["val_loss"]}
+    st.session_state['MODEL'] = model
 
 
-def plot(epochs):
+def plot_metric(model, normal_train, test_data, test_labels):
+    threshold = get_threshold(model, normal_train)
+    accuracy, precision, recall, auc = model_evaluation(model, test_data, threshold, test_labels)
+    plot_metrics(accuracy, precision, recall, auc, threshold)
+
+
+def plot_loss(epochs):
     x = np.arange(0, epochs)
     fig = go.Figure(data=go.Line(
         x=x,
