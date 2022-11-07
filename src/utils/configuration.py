d4865c934a7141380495591b1c8dcea4ca9b4f8f
koraypoyraz <koraypoyraz1@gmail.com>
2022-11-07 16:18:28 +0100

    - subclasses for different data preprocessor types - added data factory - code cleanup - layout expander for test model in evaluation page

--- src/utils/configuration.py
+++ src/utils/configuration.py
@@ -1,3 +1,5 @@
 metric_file_path = 'src/resources/metric.parquet'
 loss_file_path = 'src/resources/loss.parquet'
 parquet_engine = 'fastparquet'
+credit_data_path = 'src/resources/creditcard_data.parquet'
+ecg_data_path = 'src/resources/ecg_data.parquet'
